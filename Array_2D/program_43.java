228b29ecbe99f683d265a6ca780206d3be658c14
Yuvati Jangra <yuvatijangra@gmail.com>
2026-09-07 22:28:18 +0530

    program 43

---
+++ Array_2D/program_43.java
@@ -0,0 +1,23 @@
+//transpose of matrix 
+package Array_2D;
+
+public class program_43 {
+    public static void main(String[] args) {
+        int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+    for(int i =0; i<arr.length; i++){
+        for(int j=0; j< arr[0].length; j++){
+            int temp = arr[i][j];
+            arr[i][j] = arr[j][i];
+            arr[j][i]= temp;
+            System.out.print(arr[i][j]);
+        }
+
+      System.out.println(" ");
+    }
+    
+    }
+   
+  
+    
+}
