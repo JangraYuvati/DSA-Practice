862c099d153bf0800e6d133a42d9f4690fcff091
Yuvati Jangra <yuvatijangra@gmail.com>
2026-09-11 10:44:47 +0530

    program 49

---
+++ Recursion/program_49.java
@@ -0,0 +1,15 @@
+//print n to 1 using recursion
+
+package Recursion;
+
+public class program_49 {
+    public static void main(String[] args) {
+        print(8);
+    }
+    public static void print(int n) {
+        if(n==0) return;
+        System.out.println(n);
+        print(n-1); 
+    }
+}
+
