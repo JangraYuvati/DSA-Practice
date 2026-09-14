301658b4d302bc0a08c3f53857f50f1026860a7c
Yuvati Jangra <yuvatijangra@gmail.com>
2026-09-14 06:37:28 +0530

    program 51

---
+++ Recursion/program_51.java
@@ -0,0 +1,14 @@
+package Recursion;
+
+public class program_51 {
+    static int fact =1;
+    public static void main(String[] args) {
+        
+    }
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+}
