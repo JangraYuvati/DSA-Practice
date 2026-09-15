8dadea1b9ddd6764da8b153d63353364e8cc385a
Yuvati Jangra <yuvatijangra@gmail.com>
2026-09-15 21:16:13 +0530

    program 54

---
+++ Recursion/Recursion/program_54.java
@@ -0,0 +1,12 @@
+// nth fibonacci number
+package Recursion;
+public class program_54 {
+    public static void main(String[] args) {
+        System.out.println(fibo(5));
+    }
+    public static int fibo(int n) {
+        if(n<=1) return n;
+        return fibo(n-1)+fibo(n-2);
+        
+    }
+}
